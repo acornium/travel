32d407ee861d45236895fd92bdf5ea62f93667ea
acornium <89130305+acornium@users.noreply.github.com>
2026-04-20 14:15:29 +0300

    Fix Travelpayouts script loading

--- artifacts/travel-site/src/components/TravelpayoutsWhiteLabel.tsx
+++ artifacts/travel-site/src/components/TravelpayoutsWhiteLabel.tsx
@@ -1,25 +1,27 @@
-import { Helmet } from "react-helmet-async";
+import { useEffect } from "react";
 
-const SCRIPT_ID = "travelpayouts-white-label";
+const SCRIPT_ID = "travelpayouts-white-label-loader";
 
 export function TravelpayoutsWhiteLabel() {
+  useEffect(() => {
+    if (document.getElementById(SCRIPT_ID)) {
+      return;
+    }
+
+    const script = document.createElement("script");
+    script.id = SCRIPT_ID;
+    script.async = true;
+    script.type = "module";
+    script.src = "https://tpwgt.com/wl_web/main.js?wl_id=16502";
+    document.head.appendChild(script);
+
+    return () => {
+      script.remove();
+    };
+  }, []);
+
   return (
     <>
-      <Helmet>
-        <script id={SCRIPT_ID} type="module">
-          {`
-            if (!document.getElementById("${SCRIPT_ID}-loader")) {
-              const script = document.createElement("script");
-              script.id = "${SCRIPT_ID}-loader";
-              script.async = true;
-              script.type = "module";
-              script.src = "https://tpwgt.com/wl_web/main.js?wl_id=16502";
-              document.head.appendChild(script);
-            }
-          `}
-        </script>
-      </Helmet>
-
       <div
         id="tpwl-search"
         className="w-full rounded-2xl bg-white shadow-2xl ring-1 ring-black/5"
